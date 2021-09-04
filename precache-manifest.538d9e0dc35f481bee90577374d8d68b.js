c91c1374b5266c9d86c511e830c184c749772d0c
Abhishek <59018927+nihalkza@users.noreply.github.com>
2021-09-05 00:04:46 +0530

    Add files via upload

---
+++ precache-manifest.538d9e0dc35f481bee90577374d8d68b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26289ea94ccc867e7e17cf9c61e16e18",
+    "url": "/kanbon-board/index.html"
+  },
+  {
+    "revision": "7c390c40dea978ba2ceb",
+    "url": "/kanbon-board/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "356fde343542a3c8b4d0",
+    "url": "/kanbon-board/static/css/main.2ac9dfc0.chunk.css"
+  },
+  {
+    "revision": "7c390c40dea978ba2ceb",
+    "url": "/kanbon-board/static/js/4.28bba418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanbon-board/static/js/4.28bba418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afd15e179b6a0d0cba4",
+    "url": "/kanbon-board/static/js/5.94dad8aa.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/kanbon-board/static/js/5.94dad8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320aa04d394d08e84e3a",
+    "url": "/kanbon-board/static/js/6.1a58e712.chunk.js"
+  },
+  {
+    "revision": "356fde343542a3c8b4d0",
+    "url": "/kanbon-board/static/js/main.ea2c2ad7.chunk.js"
+  },
+  {
+    "revision": "4f2aa747eef17a39adb0",
+    "url": "/kanbon-board/static/js/runtime-main.236854e0.js"
+  },
+  {
+    "revision": "4ac1dffd526be3d81ae2",
+    "url": "/kanbon-board/static/js/stencil-h8k-logo_2-entry-js.fbdc94a6.chunk.js"
+  },
+  {
+    "revision": "8d2e25807cc1ab156efd",
+    "url": "/kanbon-board/static/js/stencil-polyfills-css-shim.774b43a6.chunk.js"
+  }
+]);
